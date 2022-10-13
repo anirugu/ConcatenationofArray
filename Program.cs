b77651c538a73ef62591bda8e9fbf75b864fa91e
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-10-13 08:52:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConcatenationofArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetConcatenation(new int[] { 1, 2, 3, });
+        }
+
+        public static int[] GetConcatenation(int[] nums)
+        {
+            int[] copied = new int[nums.Length * 2];
+            Console.WriteLine(nums.Length);
+            nums.CopyTo(copied, 0);
+            nums.CopyTo(copied, nums.Length);
+            nums = null;
+            return copied;
+        }
+    }
+}
